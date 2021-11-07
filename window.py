e6d82606961a5b7b7a02f6952dba895a7ce4e5f1
Vadym Olkhovyk <vadymolhovyk@gmail.com>
2021-11-07 14:01:56 +0200

    Update window.py

--- window.py
+++ window.py
@@ -4,6 +4,10 @@ from tkinter import *
 class Window(Tk):
     def ___init__(self):
         super().__init__()
+        Label(text="Введіть пошуковий запит:").grid(
+           row = 0, column=0, columnspan=2 
+        )
+        
 
 if __name__=="__main__":
     root = Window()
