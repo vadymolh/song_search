fa52cb289ca2cdd6832fb8b2894bf6945fa49606
Vadym Olkhovyk <vadymolhovyk@gmail.com>
2021-11-07 13:49:49 +0200

    Initial pattern of window.py

--- window.py
+++ window.py
@@ -1,1 +1,11 @@
 from tkinter import *
+
+
+class Window(Tk):
+    def ___init__(self):
+        super().__init__()
+
+if __name__=="__main__":
+    root = Window()
+    root.geometry("300x200")
+    root.mainloop()
