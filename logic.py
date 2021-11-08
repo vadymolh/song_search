744c3fe52e67dc0210523df05ce0803bc5f6d0d7
Vadym Olkhovyk <vadymolhovyk@gmail.com>
2021-11-08 19:53:39 +0200

    Add track search

--- logic.py
+++ logic.py
@@ -1,13 +1,21 @@
 #-*- coding: utf-8 -*-
+from sql_interface import DbChinook
 
 class Search_engine():
-    def __init__(self, db):
+    def __init__(self, db=DbChinook()):
         self.db = db
     def select_all_tracks(self):
         """Вибрати всі записи про треки та вивести їх"""
         res = self.db.select("""SELECT * FROM tracks;
         """)
         print (res)
+    def search_track(self, search_text):
+        #search_text = '"%'+search_text+'%"'
+        res=self.db.select('''SELECT Name FROM tracks
+            WHERE Name=?;
+        ''', search_text);
+        return res
+
 
 if __name__=="__main__":
    from sql_interface import DbChinook
--- window.py
+++ window.py
@@ -1,10 +1,11 @@
 #-*- codding:utf-8 -*-
 from tkinter import *
-
+from logic import Search_engine
 
 class Window(Tk):
     def __init__(self):
         super().__init__()
+        self.db_search = Search_engine()
         Label(self ,text="Введіть пошуковий запит:").grid(
            row = 0, column=0, columnspan=2 
         )
@@ -13,10 +14,16 @@ class Window(Tk):
                                   textvariable=self.search_text)
         self.search_but = Button(self, text="Пошук",
                                  command=self.search)
+        self.entry_search.grid(row=1, column=0,
+                               columnspan=2)
+        self.search_but.grid(row=1, column=2)
+                                   
     def search(self):
         print("Text from entry:",self.search_text.get())
-
+        text = self.search_text.get()
+        result = self.db_search.search_track(text)
+        print (result)
 if __name__=="__main__":
     root = Window()
-    root.geometry("300x200")
+    root.geometry("600x400")
     root.mainloop()
