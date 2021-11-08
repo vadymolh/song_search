4a3d6e9daf889859296d5f661b1757c1ac9a4b60
Vadym Olkhovyk <vadymolhovyk@gmail.com>
2021-11-08 19:24:18 +0200

    Add search button

--- window.py
+++ window.py
@@ -5,11 +5,16 @@ from tkinter import *
 class Window(Tk):
     def __init__(self):
         super().__init__()
-        print("test")
         Label(self ,text="Введіть пошуковий запит:").grid(
            row = 0, column=0, columnspan=2 
         )
-        
+        self.search_text = StringVar()
+        self.entry_search = Entry(self, width=60, 
+                                  textvariable=self.search_text)
+        self.search_but = Button(self, text="Пошук",
+                                 command=self.search)
+    def search(self):
+        print("Text from entry:",self.search_text.get())
 
 if __name__=="__main__":
     root = Window()
