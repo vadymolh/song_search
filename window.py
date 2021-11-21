1bfa72ed7f85154b1812be4dd0dca2df7289f937
vadymvv <vadymolhovyk@gmail.com>
2021-11-21 13:05:52 +0200

    Clear listbox

--- window.py
+++ window.py
@@ -7,26 +7,39 @@ class Window(Tk):
         super().__init__()
         self.db_search = Search_engine()
         Label(self ,text="Введіть пошуковий запит:").grid(
-           row = 0, column=0, columnspan=2 
+           row = 0, column=0, columnspan=2, rowspan=3 
         )
         self.search_text = StringVar()
         self.entry_search = Entry(self, width=60, 
                                   textvariable=self.search_text)
         self.search_but = Button(self, text="Пошук",
                                  command=self.search)
-        self.entry_search.grid(row=1, column=0,
+        self.entry_search.grid(row=3, column=0,
                                columnspan=2)
-        self.search_but.grid(row=1, column=2)
+        self.search_but.grid(row=3, column=2)
 
         self.list_track = Listbox(self, width=60, height=15)
-        self.list_track.grid(row=2, column=0, columnspan=2,
-                             sticky=W)                   
-                                   
+        self.list_track.grid(row=4, column=0, columnspan=2,
+                             rowspan=3,
+                             sticky=W)
+        self.mode_var = StringVar()
+        self.mode_var.set("tracks")
+        dict_modes = { "Search in tracks": "tracks",
+                       "Search in authors": "authors",
+                       "Search in genres": "genres"
+
+        }
+        for i, (text, val) in enumerate(dict_modes.items()):
+            r1 = Radiobutton(self, text=text,
+                              variable=self.mode_var,
+                              value=val)
+            r1.grid(row=i, column=2, sticky=W)                         
     def search(self):
         print("Text from entry:",self.search_text.get())
         text = self.search_text.get()
         result = self.db_search.search_track(text)
         print (result)
+        self.list_track.delete(0, END)
         for row in result:
             self.list_track.insert(END, row[0])
 if __name__=="__main__":
